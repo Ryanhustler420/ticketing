cc16771f7e8f23ffe3cee187488d3f4262cf2d72
Gaurav Gupta <gouravgupta840@gmail.com>
2023-06-01 00:13:13 +0530

    added bash scripts for build and push docker images

---
+++ build-images.sh
@@ -0,0 +1,6 @@
+docker build -t gupta840/ticketing-auth ./auth
+docker build -t gupta840/ticketing-orders ./orders
+docker build -t gupta840/ticketing-client ./client
+docker build -t gupta840/ticketing-tickets ./tickets
+docker build -t gupta840/ticketing-payments ./payments
+docker build -t gupta840/ticketing-expiration ./expiration
---
+++ push-images.sh
@@ -0,0 +1,6 @@
+docker push gupta840/ticketing-auth
+docker push gupta840/ticketing-orders
+docker push gupta840/ticketing-client
+docker push gupta840/ticketing-tickets
+docker push gupta840/ticketing-payments
+docker push gupta840/ticketing-expiration
